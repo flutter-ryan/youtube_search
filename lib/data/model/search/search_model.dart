bcec6a2a7910eece047401fe4b30fbd279e3c76e
flutter-ryan <ryan.achmad1785@gmail.com>
2019-11-07 14:42:37 +0800

    initial commit

---
+++ lib/data/model/search/search_model.dart
@@ -0,0 +1,6 @@
+export 'youtube_search_result.dart';
+export 'thumbnails.dart';
+export 'thumbnail.dart';
+export 'search_snippet.dart';
+export 'search_item.dart';
+export 'id.dart';
---
+++ lib/data/network/api_key.dart
@@ -0,0 +1,1 @@
+const String API_KEY = 'AIzaSyB_jZ2Q7awoP70odlCzYYC8u4FRZNpVaVM';
